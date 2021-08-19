692129447e096548041d278bc820de6aa6892b48
LuisGuilhermeAraujo <37601432+LuisGuilhermeAraujo@users.noreply.github.com>
2021-08-18 22:12:06 -0300

    commit new

--- Treino02/BuscarDados.py
+++ Treino02/BuscarDados.py
@@ -1,15 +1,13 @@
+from os import name
+from typing import List
 import  requests
 import  json
-import  csv
 import  pandas as pd
-
-
-from requests.models import Response
 
 
 headers = {
     'Accept': 'application/json',
-    'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImQyYjExYTYxLWNiOTUtNGRjMC1iZjU2LTg3YWVjOWIwZDI1OSIsImlhdCI6MTYyOTI5ODA0Niwic3ViIjoiZGV2ZWxvcGVyLzQ3OTc1ZmNlLWZlMjYtYzQzNC0wOTZlLTE2Y2RiMzBlMGEzYSIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI0NS4xNjIuMTk5LjE4NiIsIjQ1LjE2Mi4xOTkuMTg2Il0sInR5cGUiOiJjbGllbnQifV19.hrtTp12_w2ygWNV1FbyuU95TjBjBQzDp6vwNN9uNfHbCB7-Un73Q9NSreMCEcGldjKYAFTCAPsJSeVJ7rVzoLQ'
+    'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjM2Mzg3YmQxLTk1M2MtNDk0ZS1hYWQ4LWQwMzM3NDhlY2FmZSIsImlhdCI6MTYyOTMyNDQ0MSwic3ViIjoiZGV2ZWxvcGVyLzQ3OTc1ZmNlLWZlMjYtYzQzNC0wOTZlLTE2Y2RiMzBlMGEzYSIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxODkuNTQuMjIwLjIzNiIsIjE4OS41NC4yMjAuMjM2Il0sInR5cGUiOiJjbGllbnQifV19.aRqIRa251t4fyCypU8c_hmOVxpi_AnmSivQ7xHmMWujDUvpSw_PITeHuVXCgrD0WU1MQEljoXo1OVSNZRc-giQ'
 }
 
 URL_NAME = "https://api.clashroyale.com/v1/clans?name=The%20resistance"
@@ -19,23 +17,40 @@ json_itens = json.loads( result.text )
 
 
 #print(result.text)
-print( json_itens ["items"][0] )
+#print( json_itens ["items"][1] )
 for attrs in json_itens["items"]:
     if attrs["tag"].startswith( "#9V2Y" ):
         print(attrs["tag"])
-        print(attrs["name"])
-        print(attrs["clanChestLevel"])
-        print(attrs["clanWarTrophies"])
-        print(attrs["type"])
+        resultTag = attrs['tag']
 
-        Create_File = open('Create_File.csv','w', header = None, sep = ',', encoding = 'utf-8',engine='c')
-        CSV_WRITE = csv.writer(Create_File)
 
-        CSV_WRITE.writerow({"tag","name","clanChestLevel","clanWarTrophies"})
-        CSV_WRITE.writerow({attrs["tag"],attrs["name"],attrs["clanChestLevel"],attrs["clanWarTrophies"]})
+Paramet1 = resultTag
+##Replace para tirar o # para %
+resultTag = resultTag.replace("#","%23")
+print(resultTag)
 
-        Create_File.close()
 
-        break
-    else:
-        print('Tag não encontrada!')
+URL_MEMBER1 = f"https://api.clashroyale.com/v1/clans/{resultTag}/members"
+
+print(URL_MEMBER1)
+
+ResultMember1 = requests.get(URL_MEMBER1,headers=headers)
+json_Member1 = json.loads( ResultMember1.text )
+
+#print(json_Member1)
+#nome(name), - level(expLevel), - troféus(trophies), - papel(role)
+df = pd.DataFrame(columns=['name','expLevel','trophies','role'])
+for Result in json_Member1["items"]:
+    print(Result["name"])
+    print(Result["expLevel"])
+    print(Result["trophies"])
+    print(Result["role"])
+
+    df = df.append({
+     'name': Result["name"],
+     'expLevel':Result["expLevel"],
+     'trophies':Result["trophies"],
+     'role':Result["role"]
+     },ignore_index=True)
+    df.to_csv('test.csv', index=False, header=True)
+print (df)
